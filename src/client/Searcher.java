ff7deb8390b5c0cefaf21f9f5ab274fef62faf39
Eirik Klevstad <Eirik.Klevstad@gmail.com>
2015-01-25 22:03:03 -0800

    Added highlighting on keywords in documents.

--- src/client/Searcher.java
+++ src/client/Searcher.java
@@ -11,42 +11,43 @@ import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class Searcher {
   
 	public static void main(String[] args) throws MalformedURLException, SolrServerException {
 	    HttpSolrServer solr = new HttpSolrServer("http://localhost:8983/solr");
-	
+	    
+	    String q = "dinosaur";
+	    
 	    SolrQuery query = new SolrQuery();
-	    query.setQuery("*");
-	    query.setFilterQueries("April", "1992");
+	    query.setQuery(q); // title:dinosaur vil kun søke i titler f.eks
+	    //query.setFilterQueries(""); https://wiki.apache.org/solr/CommonQueryParameters
+	    //query.setFields("title"); //Her settes hvilke felt vi vil returnere i dokumentet. Blank er alt.
 	    query.setStart(0); //Startrad
 	    query.setRows(15); //maksrad
 	    query.setHighlight(true).setHighlightSnippets(1);
-	    query.setParam("hl.fl", "latest_amendment_date");
+	    query.setParam("hl.fl", "title");
+        query.setParam("hl.simple.pre", "<em>");
+        query.setParam("hl.simple.post", "</em>");
+        query.setParam("hl.fragsize", "100");
 
 	    QueryResponse response = solr.query(query);
-	    SolrDocumentList results = response.getResults();
-	    for (int i = 0; i < results.size(); ++i) {
-	      System.out.println(results.get(i));
-	    }
-	    /*
+	    
 	    Iterator<SolrDocument> iter = response.getResults().iterator();
 
 	    while (iter.hasNext()) {
 	      SolrDocument resultDoc = iter.next();
-	      
 	      String id = (String) resultDoc.getFieldValue("id");
 	      
+	      System.out.println("Found " + q + " in " + resultDoc);
 	      
-	      //String id = (String) resultDoc.getFieldValue("id"); //id is the uniqueKey field
 	      if (response.getHighlighting().get(id) != null) {
 	    	  List<String> highlightSnippets = response.getHighlighting().get(id).get("title");
-	    	  System.out.println(highlightSnippets.size());
-	    	  
+	    	  if (highlightSnippets != null){
+	    		  System.out.println("Applied highlighting: " + highlightSnippets);
+	    	  }
 	      }
-	      
-	      
-	    }*/
+	    }
 	}
 }
