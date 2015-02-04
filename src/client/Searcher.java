9a1a6e9e5d29174115e5ed55509fadb3164f3361
sivapalan <varuns@stud.ntnu.no>
2015-02-04 14:07:06 -0800

    Added query-time boost for 'Title'-field + misc minor changes

--- src/client/Searcher.java
+++ src/client/Searcher.java
@@ -5,6 +5,7 @@ import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
+
 import java.net.MalformedURLException;
 import java.util.Iterator;
 import java.util.List;
@@ -14,7 +15,7 @@ public class Searcher {
 	public static void main(String[] args) throws MalformedURLException, SolrServerException {
 	    HttpSolrServer solr = new HttpSolrServer("http://localhost:8983/solr");
 	    
-	    String q = "dinosaur";
+	    String q = "meeting";
 	    
 	    SolrQuery query = new SolrQuery();
 	    query.setQuery(q); // title:dinosaur vil kun søke i titler f.eks
@@ -27,11 +28,15 @@ public class Searcher {
 	    query.setParam("hl.simple.pre", "<em>");
 	    query.setParam("hl.simple.post", "</em>");
 	    query.setParam("hl.fragsize", "100");
-
+	    
+	    query.setParam("defType","dismax");
+	    query.setParam("qf", "id title^5.0 _type_ nsf_org latest_amendment_date file award_number award_instr prgm_manager "
+	    		+ "start_date expires expected_total_amt investigator sponsor nsf_program fld_applictn program_ref _abstract_");
+	    long t1 = System.currentTimeMillis();
 	    QueryResponse response = solr.query(query);
 	    
 	    Iterator<SolrDocument> iter = response.getResults().iterator();
-
+	    
 	    while (iter.hasNext()) {
 	      SolrDocument resultDoc = iter.next();
 	      String id = (String) resultDoc.getFieldValue("id");
@@ -45,5 +50,8 @@ public class Searcher {
 	    	  }
 	      }
 	    }
+	    long t2 = System.currentTimeMillis();
+	    System.out.println("Time used executing query and retrieving results: "+(t2-t1)+" ms.");
+	    System.out.println("Done.");
 	}
 }
